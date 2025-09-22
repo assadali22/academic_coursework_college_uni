8e2190306beb151554a14243db251a60f687ebd4
assadali22 <alia2832@outlook.com>
2025-09-22 14:41:40 -0700

    Create HexDigitToDecimal.java
    Create CS4A Intro to Java Folder.

---
+++ CS4A Intro to Java (Fall 2023 at Community College)/HexDigitToDecimal.java
@@ -0,0 +1,34 @@
+/*
+Hex system goes 0 1 2 3 4 5 6 7 8 9 A B C D E
+*/
+
+import java.util.Scanner;
+
+public class HexDigit2Dec {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a hex digit: ");
+    String hexString = input.nextLine();
+
+    // Check if the hex string has exactly one character
+    if (hexString.length() != 1) {
+      System.out.println("You must enter exactly one character");
+      System.exit(1);
+    }
+
+    // Display decimal value for the hex digit
+    char ch = Character.toUpperCase(hexString.charAt(0));
+    if ('A' <= ch && ch <= 'F') {
+      int value = ch - 'A' + 10;
+      System.out.println("The decimal value for hex digit "
+        + ch + " is " + value);
+    }
+    else if (Character.isDigit(ch)) {
+      System.out.println("The decimal value for hex digit "
+        + ch + " is " + ch);
+    }
+    else {
+      System.out.println(ch + " is an invalid input");
+    }
+  }
+}
